8a6354d0369409c12a8538cde7e2738306ec56ef
Carlos Antonio <carlosantoniooln@gmail.com>
2018-04-24 09:43:46 -0300

    Update login.js

--- Cyclonic/js/login.js
+++ Cyclonic/js/login.js
@@ -1,12 +1,4 @@
 function login(){
-	var usr = document.getElementById('usuario').value;
-	var psw = document.getElementById('senha').value;
-	if (usr == 'antonio' && psw == '123'){
-		window.location.href = 'empresa/index.html';
-		return true;
-	}else if (usr == 'martha' && psw == '123'){
-		window.location.href = 'sindico/index.html';
-		return true;
-	}
-
+	window.location.href = 'empresa/index.html';
+	return true;
 }
